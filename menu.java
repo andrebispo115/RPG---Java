12727d86766ba13d233ac4efefa2a028eac93635
agpdev2011 <palmeirinha018@gmail.com>
2025-09-10 15:11:08 -0300

    Add files via upload

---
+++ menu.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+  
+ public class menu {
+
+
+    public static void main(String[] args){
+    
+        Scanner teclado = new Scanner(System.in);
+        int escolha = 3;
+        do{
+        System.out.println("1-jogar");
+        System.out.println("2-creditos");
+        System.out.println("3-sair");
+        escolha = teclado.nextInt();
+        } while(escolha != 3);
+
+    }
+}   
