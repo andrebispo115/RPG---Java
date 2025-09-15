f64084c554dc7b66e52e76a26e45b2750f8f75d8
AndreBispo <andresvale2006@gmail.com>
2025-09-15 16:28:09 -0300

    Create Escolherclasse.java -- teste

---
+++ Escolherclasse.java
@@ -0,0 +1,36 @@
+    import java.util.Scanner; // Passo 1: Importar a classe
+    
+public class Escolherclasse(){
+    
+    public static void main(String[] args) {
+        
+        // Passo 2: Criar um objeto Scanner
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Escolha uma classe");
+        System.out.println("1 - Sabre");
+        System.out.println("2 - lanceiro");
+        System.out.println("3 - tanque");
+
+        // Passo 3: Ler a resposta do usuário e armazenar em uma variável
+        int escilher = entrada.nextInt(); 
+        
+        // Aqui, 'escolher' armazena o número digitado (1, 2 e 3)
+        
+        // Agora, você pode usar a variável para controlar o fluxo do programa
+        if (escolher == 1) {
+            System.out.println("Você escolheu o sabre");
+            
+        } else if (escolher == 2) {
+            System.out.println("Você escolheu o lanceiro");
+            
+        } else if (escolher == 3) {
+            System.out.println("Voçê escolheu o tanque");
+        }else {
+            System.out.println("Ação inválida, tente novamente.");
+        }
+        
+        // Sempre feche o Scanner para liberar recursos
+        entrada.close(); 
+    }
+}
