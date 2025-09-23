15667aee429f28bd38a56135c16b9b5b85972406
AndreBispo <andresvale2006@gmail.com>
2025-09-23 20:41:32 -0300

    Create Ladrao.java

---
+++ Ladrao.java
@@ -0,0 +1,4 @@
+public class Ladrao extends Lutador(){
+    public Ladrao(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
+        super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
+}
