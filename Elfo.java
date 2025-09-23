6c9ea357dac67302eecaa9817288fa453d18b7ed
AndreBispo <andresvale2006@gmail.com>
2025-09-23 20:40:29 -0300

    Create Elfo.java

---
+++ Elfo.java
@@ -0,0 +1,4 @@
+public class Elfo extends Lutador(){
+    public Elfo(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
+        super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
+}
