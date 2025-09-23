3e2f9d2ced59eb6a977ba4b10c43e16daf655818
AndreBispo <andresvale2006@gmail.com>
2025-09-23 19:59:30 -0300

    Update Inimigo.java

--- Inimigo.java
+++ Inimigo.java
@@ -1,5 +1,5 @@
 public class Inimigo extends Lutador {
-    public Inimigo(String nome, int vida, int ataque, int defesa, int agilidade) {
-        super(nome, vida, ataque, defesa, agilidade);
+    public Inimigo(String nome, int vida, int forca, int defesa, int agilidade) {
+        super(nome, vida, forca, defesa, agilidade);
     }
 }
