43061086b3c77c6422a96ae3dced6a70d400e2e6
AndreBispo <andresvale2006@gmail.com>
2025-09-23 20:41:00 -0300

    Create Guerreiro.java

---
+++ Guerreiro.java
@@ -0,0 +1,4 @@
+public class Guerreiro extends Lutador(){
+    public Guerreiro(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
+        super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
+}
