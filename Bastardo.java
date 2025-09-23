20f724c23ada5a1a8ceda850556b61427b8d5163
AndreBispo <andresvale2006@gmail.com>
2025-09-23 20:41:59 -0300

    Create Bastardo.java

---
+++ Bastardo.java
@@ -0,0 +1,4 @@
+public class Bastardo extends Lutador(){
+    public Bastardo(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
+        super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
+}
