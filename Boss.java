7f8efcf61f18cc0843a9587a2edcf30bbcf09a52
AndreBispo <andresvale2006@gmail.com>
2025-09-23 20:01:39 -0300

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -1,4 +1,4 @@
 public class Boss extends Inimigo(){
-    public Boss(String nome, int vida, int ataque, int defesa, int agilidade) {
-        super(nome, vida, ataque, defesa, agilidade);
+    public Boss(String nome, int vida, int forca, int defesa, int agilidade) {
+        super(nome, vida, forca, defesa, agilidade);
 }
