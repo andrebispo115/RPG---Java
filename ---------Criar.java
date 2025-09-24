5fa1a0d34bf13510e07b08dcc5108951d0cd0af2
agpdev2011 <palmeirinha018@gmail.com>
2025-09-24 17:50:31 -0300

    Update ---------Criar.java

--- ---------Criar.java
+++ ---------Criar.java
@@ -22,4 +22,19 @@ public class Criar {
             " | Defesa: " + defesa + 
             " | Agilidade: " + agilidade);
     }
+
+    // Verifica se está vivo
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    // Atacar outro personagem
+    public void atacar(Criar inimigo) {
+        int dano = this.ataque - inimigo.defesa;
+        if (dano < 0) dano = 0; // impede dano negativo
+        inimigo.vida -= dano;
+        if (inimigo.vida < 0) inimigo.vida = 0; // evita vida negativa
+
+        System.out.println(nome + " atacou " + inimigo.nome + " causando " + dano + " de dano!");
+    }
 }
