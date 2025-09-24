0a2c973896b233a01984565a5d566898fc40cf26
agpdev2011 <palmeirinha018@gmail.com>
2025-09-24 17:39:35 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,4 +1,5 @@
-public class Guerreiro extends Lutador(){
-    public Guerreiro(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
-        super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
+public class Guerreiro extends Criar{
+    public Guerreiro(){
+        super("Guerreiro", 80, 20, 20,2);
+    }
 }
