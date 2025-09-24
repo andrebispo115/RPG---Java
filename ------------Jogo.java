a20a733326f684ab72ad591faa64aeb6f191faf6
agpdev2011 <palmeirinha018@gmail.com>
2025-09-24 17:49:00 -0300

    Update ------------Jogo.java

--- ------------Jogo.java
+++ ------------Jogo.java
@@ -2,26 +2,61 @@ import java.util.Scanner;
 
 public class Jogo {
     public static void main(String[] args) {
-        
-        System.out.println("Escolha a sua classe");
-        
+        Scanner sc = new Scanner(System.in);
 
-        
+        // Escolher classe do herói
+        System.out.println("ESCOLHA SUA CLASSE: GUERREIRO, ELFO, LADRAO"); 
+        String escolha = sc.nextLine().toLowerCase();
+
+        Criar heroi;
+
+        switch (escolha) {
+            case "guerreiro":
+                heroi = new Criar("Guerreiro", 80, 20, 10, 2);
+                break;
+            case "elfo":
+                heroi = new Criar("Elfo", 55, 17, 15, 10);
+                break;
+            case "ladrao":
+                heroi = new Criar("Ladrao", 75, 10, 9, 30);
+                break;
+            default:
+                System.out.println("Classe inválida! Você será um Bastardo.");
+                heroi = new Criar("Bastardo", 30, 5, 2, 4);
+        }
+
+        // Criar inimigo
+        Criar orc = new Criar("Orc", 60, 15, 5, 5);
+
+        System.out.println("\n-- INICIO DA BATALHA --");
+        heroi.mostrarStatus();
+        orc.mostrarStatus();
+
+        // Loop de combate
         while (heroi.estaVivo() && orc.estaVivo()) {
-            // Lógica do turno do herói
-            // Solicitar a ação do jogador (atacar ou defender)
+            System.out.println("\nSeu turno! (1 - Atacar)");
+            int acao = sc.nextInt();
 
-            // Lógica do turno do inimigo
-            // Gerar aleatoriamente a ação do inimigo (atacar ou defender)
+            if (acao == 1) {
+                heroi.atacar(orc);
+            }
 
-            // Exibir o estado da batalha (vida, etc.)
+            if (orc.estaVivo()) {
+                orc.atacar(heroi);
+            }
+
+            // Mostrar status após cada turno
+            heroi.mostrarStatus();
+            orc.mostrarStatus();
         }
 
-        // Lógica para determinar quem venceu
+        // Resultado final
         if (heroi.estaVivo()) {
-            System.out.println("Parabéns, você derrotou o orc!");
+            System.out.println("\nVocê venceu!");
         } else {
-            System.out.println("Você foi derrotado!");
+            System.out.println("\nVocê foi derrotado!");
         }
+
+        sc.close();
     }
 }
