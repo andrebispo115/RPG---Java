5a40708d0739bb8cce4c540e946bb0ff20700f62
AndreBispo <andresvale2006@gmail.com>
2025-09-27 15:41:27 -0300

    Update Ladrao.java

--- Ladrao.java
+++ Ladrao.java
@@ -1,4 +1,4 @@
-public class Ladrao extends Lutador(){
+public class Ladrao extends Heroi(){
     public Ladrao(String nome, int vida, int forca, int defesa, int agilidade, int constituicao, int destreza) {
         super(nome, vida, forca, defesa, agilidade, constituicao, destreza);
 }
