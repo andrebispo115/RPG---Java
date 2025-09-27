cd52f0597c9d89ee4275a41a635f1d3e6c1847c4
AndreBispo <andresvale2006@gmail.com>
2025-09-27 17:02:22 -0300

    Update Armadura.java

--- Armadura.java
+++ Armadura.java
@@ -1,1 +1,16 @@
-
+public class Armadura{
+    private String nome;
+    private int defesa;
+    
+    Armadura(String nome){
+        this.nome = nome;
+    }
+    
+    public double getdefesa {
+        return this.defesa;
+    }
+    
+    public void calcularDefesa(){
+        this.defesa = 10 + (1,5 * this.constituicao);
+    }
+}
