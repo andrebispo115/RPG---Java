2526e690df4661e7557e1ab7251023a1b718f409
AndreBispo <andresvale2006@gmail.com>
2025-09-27 16:44:13 -0300

    Update Arma.java

--- Arma.java
+++ Arma.java
@@ -3,13 +3,11 @@ import java.util.Random;
 public class Arma extends Dado {
     private String nome;
     private String categoria;
-    private double constanteDeDano;
     private double dano;
 
-    public Arma(String nome, String categoria, double constanteDeDano) {
+    public Arma(String nome, String categoria) {
         this.nome = nome;
         this.categoria = categoria;
-        this.constanteDeDano = constanteDeDano;
     }
 
     public double getDano() {
@@ -19,12 +17,12 @@ public class Arma extends Dado {
     public void calcularDano(d) {
         if (this.categoria.equals("Pesada")) {
             int resultadoDado = rolarDado(12);
-            this.dano = (this.forca * 1.5) + this.constanteDeDano + resultadoDado;
+            this.dano = (this.forca * 1.5) + 5 + resultadoDado;
         } else { // Categoria "Leve"
             int resultadoD6_1 = rolarDado(6);
             int resultadoD6_2 = rolarDado(6);
             int resultadoD4 = rolarDado(4);
-            this.dano = resultadoD6_1 + resultadoD6_2 + resultadoD4 + this.destreza + this.constanteDeDano;
+            this.dano = resultadoD6_1 + resultadoD6_2 + resultadoD4 + this.destreza + 5;
         }
     }
 }
