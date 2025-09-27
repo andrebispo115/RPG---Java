b409c53eb977a8b5ec85ec51746c023959348cb4
AndreBispo <andresvale2006@gmail.com>
2025-09-27 16:06:08 -0300

    Delete Inimigo.java

--- Inimigo.java
+++
@@ -1,5 +0,0 @@
-public class Inimigo extends Lutador {
-    public Inimigo(String nome, int vida, int forca, int defesa, int agilidade) {
-        super(nome, vida, forca, defesa, agilidade);
-    }
-}
