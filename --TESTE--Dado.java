cfd6c1fad3c72bf7979659b8a830bfd8c8f8ebf5
AndreBispo <andresvale2006@gmail.com>
2025-09-27 21:24:40 -0300

    Update --TESTE--Dado.java

--- --TESTE--Dado.java
+++ --TESTE--Dado.java
@@ -1,19 +1,10 @@
 import java.util.Random;
 
 public class Dado {
-
     public int rolarDado(int lado) {
-        // Cria um objeto da classe Random
         Random gerador = new Random();
-
-        // Gera um número aleatório entre 0 (inclusive) e o lado max. do dado (exclusive)
-        int numeroAleatorio = gerador.nextInt(lado);
-
-        // Para obter o intervalo de 1 a lado max. do dado, somamos 1 ao resultado.
-        int resultado = numeroAleatorio + 1;
-
-        System.out.println("D" + lado + ": " + resultado); // Sintaxe corrigida
-        
-        return resultado; // Retorna o valor da rolagem
+        int resultado = gerador.nextInt(lado) + 1;
+        System.out.println("Rolou um D" + lado + ": " + resultado);
+        return resultado;
     }
 }
