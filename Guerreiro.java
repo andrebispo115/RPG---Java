8a9af9e0c3b5f3fb7b5bf9d2077312b80658048a
AndreBispo <andresvale2006@gmail.com>
2025-09-27 21:19:30 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,5 +1,6 @@
-public class Guerreiro extends Criar{
-    public Guerreiro(){
+public class Guerreiro extends Criar {
+    public Guerreiro() {
         super("Guerreiro", 80, 20, 20, 2, 20, 50);
     }
 }
+
