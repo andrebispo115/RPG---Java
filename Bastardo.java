59d58736b84661dab227bb486487c4a17f1fdbad
AndreBispo <andresvale2006@gmail.com>
2025-10-03 15:55:22 -0300

    Update and rename Bastardo.java to Claus.java

--- Bastardo.java
+++
@@ -1,5 +0,0 @@
-public class Bastardo extends Criar {
-    public Bastardo() {
-        super("Bastardo", 30, 6, 2, 8, 4, 6);
-    }
-}
---
+++ Claus.java
@@ -0,0 +1,5 @@
+public class Claus extends Criar {
+    public Claus() {
+        super("Claus", 30, 6, 2, 8, 4, 6);
+    }
+}
