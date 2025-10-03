47e5585f452edf6b2460e95751d2aca486689865
AndreBispo <andresvale2006@gmail.com>
2025-10-03 15:54:47 -0300

    Update and rename Ladrao.java to Takeo.java

--- Ladrao.java
+++
@@ -1,5 +0,0 @@
-public class Ladrao extends Criar {
-    public Ladrao() {
-        super("Ladrao", 50, 30, 15, 70, 70, 15);
-    }
-}
---
+++ Takeo.java
@@ -0,0 +1,5 @@
+public class Takeo extends Criar {
+    public Takeo() {
+        super("Takeo", 50, 30, 15, 70, 70, 15);
+    }
+}
