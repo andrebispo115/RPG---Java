a3dc0ca26ea0b98794d11d7d3226625a1b27f1af
AndreBispo <andresvale2006@gmail.com>
2025-10-03 15:54:16 -0300

    Update and rename Guerreiro.java to Nikolai.java

--- Guerreiro.java
+++
@@ -1,6 +0,0 @@
-public class Guerreiro extends Criar {
-    public Guerreiro() {
-        super("Guerreiro", 80, 20, 20, 2, 20, 50);
-    }
-}
-
---
+++ Nikolai.java
@@ -0,0 +1,6 @@
+public class Nikolai extends Criar {
+    public Nikolai() {
+        super("Nikolai", 80, 20, 20, 2, 20, 50);
+    }
+}
+
