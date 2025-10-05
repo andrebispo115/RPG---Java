fd358c7123630b11a44a6421101e5f4b11093374
AndreBispo <andresvale2006@gmail.com>
2025-10-05 14:17:03 -0300

    Update Inimigo.java

--- Inimigo.java
+++ Inimigo.java
@@ -1,5 +1,11 @@
-public class Inimigo extends Criar {
+import java.util.Random;
+
+public class Inimigo extends Personagem {
     public Inimigo(String nome, int vida, int forca, int defesa, int agilidade, int destreza, int constituicao) {
         super(nome, vida, forca, defesa, agilidade, destreza, constituicao);
+    }
+    
+    public int decidirAcao() {
+        return dado.rolarDado(3); 
     }
 }
