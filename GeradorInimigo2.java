68f8a8918f241c8d8e9fdaf163a43b3c646fade3
AndreBispo <andresvale2006@gmail.com>
2025-10-05 14:15:01 -0300

    Update GeradorInimigo2.java

--- GeradorInimigo2.java
+++ GeradorInimigo2.java
@@ -2,20 +2,17 @@ import java.util.Random;
 
 public class GeradorInimigo2 {
 
+    private static final Inimigo[] INIMIGOS_DO_CAPITULO_2 = { 
 
-    private static final Inimigo[] INIMIGOS_DO_CAPITULO_1 = {
+        new Inimigo("Carniçal de Éter", 45, 15, 6, 4, 15, 25),
 
-        new Inimigo("Carniçal de Éter", 45, 12, 6, 4, 2, 25),
-        
-        new Inimigo("Espectro da Sombra", 35, 15, 2, 15, 8, 30)
+        new Inimigo("Espectro da Sombra", 35, 20, 2, 15, 20, 30)
     };
     
     private static final Random random = new Random();
-  
-    public static Inimigo gerarPrimeiroInimigo2() {
-
-        int indiceAleatorio = random.nextInt(INIMIGOS_DO_CAPITULO_1.length);
-
-        return INIMIGOS_DO_CAPITULO_2[indiceAleatorio];
+    
+    public static Inimigo gerarSegundoInimigo() { 
+        int indiceAleatorio = random.nextInt(INIMIGOS_DO_CAPITULO_2.length);
+        return INIMIGOS_DO_CAPITULO_2[indiceAleatorio]; 
     }
 }
